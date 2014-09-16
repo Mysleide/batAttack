df7a87e10965161e6491d9314ba59219de879747
Mysleide <mysleide@gmail.com>
2014-09-16 13:55:10 -0300

    versao_inicial
    Primeira versão do batAttack

---
+++ main.lua
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+
+display.setStatusBar(display.HiddenStatusBar)
+CoronaLogo = display.newImage("batAttack.png")
+
+	  CoronaLogo.anchorX = 0;
+      CoronaLogo.anchorY = 0;
+	  CoronaLogo.x = 0;
+      CoronaLogo.y = 0;
